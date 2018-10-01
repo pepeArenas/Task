68c67ce436166122c3a4e1f8abd7b11cf20364c8
Jose de Jesus Arenas <jarenas@Joses-MacBook-Pro.local>
2018-10-01 10:40:16 -0700

    Primera version de task

--- Management/src/main/java/com/stack/jspdemo/ManagementAppApplication.java
+++ Management/src/main/java/com/stack/jspdemo/ManagementAppApplication.java
@@ -1,12 +1,12 @@
-package com.skt.task;
+package com.stack.jspdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagementApplication {
+public class ManagementAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
+		SpringApplication.run(ManagementAppApplication.class, args);
 	}
 }
