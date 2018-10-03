15f5c48d782496839e6e94278042635d4031ad96
jesus <jesusarenasm>
2018-10-03 10:42:39 -0700

    Firt version of product microservice

--- ProductApp/src/main/java/com/stack/jspdemo/service/ProductService.java
+++ ProductApp/src/main/java/com/stack/jspdemo/service/ProductService.java
@@ -1,35 +1,32 @@
 package com.stack.jspdemo.service;
 
-import org.springframework.stereotype.Component;
-
-import com.stack.jspdemo.model.Product;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.stack.jspdemo.model.Product;
+import com.stack.jspdemo.repository.ProductRepository;
+
 @Component
 public class ProductService {
-    public static List<Product> products = new ArrayList<>();
+	public static List<Product> products = new ArrayList<>();
 
-    private static int userCount = 3;
+	private static int userCount = 3;
 
-    static {
-        products.add(new Product(1, "tuerca","modelo1", "20,20"));
-        products.add(new Product(2, "tornillo","modelo2", "40,20"));
-        products.add(new Product(3, "destornillador","modelo3", "90,20"));
-    }
+	@Autowired
+	private ProductRepository repo;
 
-    public List<Product> findAll() {
+	public List<Product> findAll() {
 
-        return products;
-    }
+		return repo.getAllProducts();
+	}
 
-    public Product save(Product product) {
-        if (product.getId() == null) {
+	public Product save(Product product) {
+		// products.add(product);
+		repo.insertProduct(product);
 
-            product.setId(++userCount);
-        }
-        products.add(product);
-        return product;
-    }
+		return product;
+	}
 }
